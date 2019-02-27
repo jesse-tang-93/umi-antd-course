73b1afe9c60bd9ee2bdb607c34c303233d283b85
tangxq <tangxiaoqiang@chinawbb.com>
2019-02-27 17:31:13 +0800

    理解props.children

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 export default {
   plugins:[
-    ['umi-plugin-react',{}]
+    ['umi-plugin-react',{antd:true}]
   ],
   singular: true, // 让存放页面的文件夹允许为page ，单数。因为。umi中约定存放页面代码的文件夹是pages
   routes:
